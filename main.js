f0af93c7460101001348ff4e85ee2cc2ee53757b
Gary Sun <sungarysun@gmail.com>
2020-12-03 22:38:24 -0800

    test

--- main.js
+++ main.js
@@ -21,11 +21,11 @@ class test {
         if(v == "SEARCH"){
             document.getElementById("SELECTERROR").innerHTML = "";
             var input1 = document.getElementById("input1");
-            var input2 = document.getElementById("input2");
-            var input3 = document.getElementById("input3");
-            var input4 = document.getElementById("input4");
-            var input5 = document.getElementById("input5");
-            SEARCH(input1.value,input2.value,input3.value,input4.value,input5.value)
+            // var input2 = document.getElementById("input2");
+            // var input3 = document.getElementById("input3");
+            // var input4 = document.getElementById("input4");
+            // var input5 = document.getElementById("input5");
+            // SEARCH(input1.value,input2.value,input3.value,input4.value,input5.value)
             alert(input1.value);
         } else if(v == "ADD"){
             document.getElementById("SELECTERROR").innerHTML = "";
